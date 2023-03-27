91ec1d4a485c99ee89cdc1151bac578c969dbafa
Madoura <madouura@gmail.com>
2023-03-27 09:01:47 -0500

    hosts: add more fonts

--- hosts/profiles/system/fonts.nix
+++ hosts/profiles/system/fonts.nix
@@ -1,7 +1,20 @@
 { pkgs, ... }: {
-  fonts.fonts = with pkgs; [
-    ipafont
-    baekmuk-ttf
-    nerdfonts
-  ];
+  fonts = {
+    fontDir.enable = true;
+    fontconfig.enable = true;
+
+    fonts = with pkgs; [
+      nerdfonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-code-pro
+      source-han-mono
+      source-han-sans
+      source-han-serif
+      ipafont
+      baekmuk-ttf
+      wqy_zenhei
+    ];
+  };
 }
