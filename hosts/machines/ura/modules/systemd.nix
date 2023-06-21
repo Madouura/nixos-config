c733e4914396a4e9fd9b0dba40265ababbb34a96
Madoura <madouura@gmail.com>
2023-06-20 20:43:04 -0500

    hosts: fix cpu isolation for VM

--- hosts/machines/ura/modules/systemd.nix
+++ hosts/machines/ura/modules/systemd.nix
@@ -1,15 +1,13 @@
 { pkgs,  ... }: let
   qemuHook = pkgs.writeShellScript "qemu-hook.sh" ''
     if [ "$1" == "win11" ]; then
-      if [ "$2" == "prepare" ]; then
-        systemctl set-property --runtime -- user.slice AllowedCPUs=0-7,16-23
+      if [ "$2" == "started" ]; then
         systemctl set-property --runtime -- system.slice AllowedCPUs=0-7,16-23
+        systemctl set-property --runtime -- user.slice AllowedCPUs=0-7,16-23
         systemctl set-property --runtime -- init.scope AllowedCPUs=0-7,16-23
-      fi
-
-      if [ "$2" == "release" ]; then
-        systemctl set-property --runtime -- user.slice AllowedCPUs=0-31
+      elif [ "$2" == "release" ]; then
         systemctl set-property --runtime -- system.slice AllowedCPUs=0-31
+        systemctl set-property --runtime -- user.slice AllowedCPUs=0-31
         systemctl set-property --runtime -- init.scope AllowedCPUs=0-31
       fi
     fi
