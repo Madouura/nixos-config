efd82ac6656c13cad38b09e691f018b0cceadf1b
Madoura <madouura@gmail.com>
2023-08-03 08:47:17 -0500

    hosts: fix QT applications

--- hosts/machines/ura/modules/environment.nix
+++ hosts/machines/ura/modules/environment.nix
@@ -14,6 +14,7 @@
     variables = {
       EDITOR = "nano";
       VISUAL = "codium";
+      QT_QPA_PLATFORM = "wayland"; # Fix QT applications TODO: Remove when fixed upstream
     };
 
     etc = {
