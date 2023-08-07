76f37cafa03d9ea18c54b9eccb637e8a9ee6b628
Madoura <madouura@gmail.com>
2023-08-07 08:12:36 -0500

    hosts: clarify why ondemand governor is not available

--- hosts/machines/ura/modules/powerManagement.nix
+++ hosts/machines/ura/modules/powerManagement.nix
@@ -1,3 +1,3 @@
 {
-  powerManagement.cpuFreqGovernor = "performance"; # ondemand is missing?
+  powerManagement.cpuFreqGovernor = "performance"; # ondemand is missing because of amd_pstate, intentional
 }
