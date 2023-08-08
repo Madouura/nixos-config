55b9feea08d00f3dbc1677d4e67e19a3cae88c95
Madoura <madouura@gmail.com>
2023-08-07 21:19:04 -0500

    hosts: try disabling gamemode

--- hosts/machines/ura/modules/programs.nix
+++ hosts/machines/ura/modules/programs.nix
@@ -27,18 +27,18 @@
       settings.command_timeout = 1000;
     };
 
-    gamemode = {
-      enable = true;
-
-      settings = {
-        general.renice = 10;
-
-        gpu = {
-          apply_gpu_optimisations = "accept-responsibility";
-          gpu_device = 0;
-          amd_performance_level = "high";
-        };
-      };
-    };
+    # gamemode = {
+    #   enable = true;
+# 
+    #   settings = {
+    #     general.renice = 10;
+# 
+    #     gpu = {
+    #       apply_gpu_optimisations = "accept-responsibility";
+    #       gpu_device = 0;
+    #       amd_performance_level = "high";
+    #     };
+    #   };
+    # };
   };
 }
